f3ec1b11f458e06862fb4063e798af682a15be78
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2022-11-02 10:57:36 +0100

    zadanie 1

---
+++ Skrypt.R
@@ -0,0 +1,16 @@
+dane_kabanos <- read.csv("/Users/karolstudniarek/Desktop/REPOZYTORIA/kabanosProjekt/csvki/tar_d_compared_with_mak.csv")
+dane_makaron <- read.csv("/Users/karolstudniarek/Desktop/REPOZYTORIA/kabanosProjekt/csvki/mak_d_compared_with_tar.csv")
+
+# zad1 
+
+#kabanos_zamk <- dane_kabanos$Zamkniecie
+#makaron_zamk <- dane_makaron$Zamkniecie
+
+kabanos_zamk <- dane_kabanos[,'Zamkniecie']
+makaron_zamk <- dane_makaron[,'Zamkniecie']
+
+plot(kabanos_zamk)
+plot(makaron_zamk)
+
+hist(kabanos_zamk, prob=TRUE)
+hist(makaron_zamk, prob=TRUE)
