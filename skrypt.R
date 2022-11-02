58b02ab7c54a186fac63c338fd8e7f4b54c108d4
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2022-11-02 11:20:39 +0100

    zad2 /brakuje interpretacji slownej

--- skrypt.R
+++ skrypt.R
@@ -1,3 +1,6 @@
+library(e1071)
+
+
 dane_kabanos <- read.csv("kabanosProjekt/csvki/tar_d_compared_with_mak.csv")
 dane_makaron <- read.csv("kabanosProjekt/csvki/mak_d_compared_with_tar.csv")
 
@@ -16,5 +19,22 @@ hist(kabanos_zamk, prob=TRUE)
 hist(makaron_zamk, prob=TRUE)
 
 # zad2
+
+mean(kabanos_zamk)
+mean(makaron_zamk)
+
+sd(kabanos_zamk)
+sd(makaron_zamk)
+
+skewness(kabanos_zamk)
+skewness(makaron_zamk)
+
+kurtosis(kabanos_zamk)
+kurtosis(makaron_zamk)
+
+# zad3
+
+s
+
 
 
